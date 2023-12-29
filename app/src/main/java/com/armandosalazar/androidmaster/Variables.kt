e4688bd050d650681d804b27a9d06130f595b5ee
Armando Salazar <armandosalazar351@gmail.com>
2023-12-29 00:04:14 -0600

    feat: add single expression function

--- app/src/main/java/com/armandosalazar/androidmaster/Variables.kt
+++ app/src/main/java/com/armandosalazar/androidmaster/Variables.kt
@@ -1,10 +1,14 @@
 package com.armandosalazar.androidmaster
 
+// Global Variables
+const val globalName = "Armando Salazar" // const is a compile-time constant like static in Java
+
+// Main Function
 fun main() {
     // Variables & Constants (Values)
-    var name = "Armando"
+    var name = "Armando" // var is a mutable variable like var in Java
     name = "Armando Salazar"
-    val age: Int = 23
+    val age: Int = 23 // val is a runtime constant like final in Java
     val example: Long = 23L
     val floatExample: Float = 23.0F
     val doubleExample: Double = 23.0
@@ -18,9 +22,12 @@ fun main() {
 
     print("Hi, my name is $name and I'm $age years old.\n")
     println("The sum of 2 and 3 is ${sum(2.toDouble(), 3.toDouble())}")
+    println("The sub of 2 and 3 is ${sub(2.toDouble(), 3.toDouble())}")
 }
 
 // Functions
-fun sum(a: Double, b: Double): Double {
+fun sum(a: Double, b: Double): Double { // Function with return value
     return a + b
 }
+
+fun sub(a: Double, b: Double): Double = a - b // Single Expression Function
