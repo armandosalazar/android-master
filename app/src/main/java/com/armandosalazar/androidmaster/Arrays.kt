7af015587e7c42f57692ed49d4a3cbf7b04612f7
Armando Salazar <armandosalazar351@gmail.com>
2023-12-29 00:29:06 -0600

    feat: add arrays and fors

---
+++ app/src/main/java/com/armandosalazar/androidmaster/Arrays.kt
@@ -0,0 +1,27 @@
+package com.armandosalazar.androidmaster
+
+fun main() {
+    val daysOfWeek =
+        arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+    daysOfWeek[0] = "MONDAY"
+
+    println(daysOfWeek[0])
+    println(daysOfWeek.size)
+
+    for (day in daysOfWeek) {
+        println(day)
+    }
+
+    for (index in daysOfWeek.indices) {
+        println(daysOfWeek[index])
+    }
+
+    daysOfWeek.forEach { day ->
+        println(day)
+    }
+
+    daysOfWeek.forEachIndexed { index, day ->
+        println("$index: $day")
+    }
+}
