36417764e2dd2eb2ceaad3f1e203116f36375518
Armando Salazar <armandosalazar351@gmail.com>
2023-12-29 00:07:31 -0600

    feat: add conditionals

---
+++ app/src/main/java/com/armandosalazar/androidmaster/Conditionals.kt
@@ -0,0 +1,11 @@
+package com.armandosalazar.androidmaster
+
+fun main() {
+    val isSunny = true
+
+    if (isSunny) {
+        println("It's sunny!")
+    } else {
+        println("It's not sunny!")
+    }
+}
