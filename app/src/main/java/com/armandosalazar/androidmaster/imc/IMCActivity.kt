a381c07481692d8c2713f02c8e50d5e80fff3b15
Armando Salazar <armandosalazar351@gmail.com>
2024-01-06 21:00:07 -0600

    feat: add range slider and display height

--- app/src/main/java/com/armandosalazar/androidmaster/imc/IMCActivity.kt
+++ app/src/main/java/com/armandosalazar/androidmaster/imc/IMCActivity.kt
@@ -2,12 +2,17 @@ package com.armandosalazar.androidmaster.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.armandosalazar.androidmaster.R
+import com.google.android.material.slider.RangeSlider
 
 class IMCActivity : AppCompatActivity() {
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+    private var isMale = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +25,13 @@ class IMCActivity : AppCompatActivity() {
     private fun initComponent() {
         viewMale = findViewById(R.id.cardViewMale)
         viewFemale = findViewById(R.id.cardViewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun setListeners() {
         viewMale.setOnClickListener {
+            isMale = true
             viewMale.setCardBackgroundColor(
                 resources.getColor(
                     R.color.background_component_selected,
@@ -38,6 +46,7 @@ class IMCActivity : AppCompatActivity() {
             )
         }
         viewFemale.setOnClickListener {
+            isMale = false
             viewFemale.setCardBackgroundColor(
                 resources.getColor(
                     R.color.background_component_selected,
@@ -50,6 +59,10 @@ class IMCActivity : AppCompatActivity() {
                     null
                 )
             )
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            "${value.toInt()} cm".also { tvHeight.text = it }
         }
     }
 }
